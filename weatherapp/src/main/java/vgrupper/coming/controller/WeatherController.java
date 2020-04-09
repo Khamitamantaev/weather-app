f580eabb92f075cd2ef419bbc62b5d853d55d9e2
Khamit <khamitamantaev@gmail.com>
2020-04-10 00:08:59 +0500

    restcontoller simple and WeatherPrognoz для списка сущностей с внедренными аннотациями

---
+++ weatherapp/src/main/java/vgrupper/coming/controller/WeatherController.java
@@ -0,0 +1,11 @@
+package vgrupper.coming.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherController {
+
+
+}
