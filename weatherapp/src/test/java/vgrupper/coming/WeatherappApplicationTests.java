ba6939755d9c3983f286f6989b04724cfdf34b3e
Khamit <khamitamantaev@gmail.com>
2020-04-09 22:01:14 +0500

    project has been created!(c) Khamit

---
+++ weatherapp/src/test/java/vgrupper/coming/WeatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package vgrupper.coming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
